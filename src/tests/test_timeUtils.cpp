4b3f7caebb7ae00456ef4345a97e52d15079e3c3
Maxim Kandrushin <maxim123.321.2014.2@yandex.ru>
2025-01-18 11:25:43 +0300

    Add google tests

---
+++ src/tests/test_timeUtils.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "timeUtils.h"
+
+TEST(TimeUtilsTest, TimeParserHHMMtoM) {
+    EXPECT_EQ(timeParserHHMMtoM("12:34"), 754);
+    EXPECT_EQ(timeParserHHMMtoM("00:00"), 0);
+    EXPECT_EQ(timeParserHHMMtoM("23:59"), 1439);
+}
+
+TEST(TimeUtilsTest, TimeParserMtoHHMM) {
+    EXPECT_EQ(timeParserMtoHHMM(754), "12:34");
+    EXPECT_EQ(timeParserMtoHHMM(0), "00:00");
+    EXPECT_EQ(timeParserMtoHHMM(1439), "23:59");
+}
+
+TEST(TimeUtilsTest, RoundToNearestHour) {
+    EXPECT_EQ(roundToNearestHour(754), 780);  // 13:00
+    EXPECT_EQ(roundToNearestHour(0), 0);      // 00:00
+    EXPECT_EQ(roundToNearestHour(1439), 1440); // 24:00
+}
